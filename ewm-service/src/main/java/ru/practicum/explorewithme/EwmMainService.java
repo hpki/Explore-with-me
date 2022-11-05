fa3d67e64e12cc665cb4289d35d231ff48568fc3
Konstantin <pronyuk.k@yandex.ru>
2022-11-05 20:28:48 +0300

    исправить ошибки

--- ewm-service/src/main/java/ru/practicum/explorewithme/EwmMainService.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/EwmMainService.java
@@ -2,6 +2,8 @@ package ru.practicum.explorewithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EwmMainService {
@@ -9,4 +11,10 @@ public class EwmMainService {
     public static void main(String[] args) {
         SpringApplication.run(EwmMainService.class, args);
     }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 }
